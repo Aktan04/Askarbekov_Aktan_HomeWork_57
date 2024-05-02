31a91810462bed07ed0d92dd27036f5970c0477b
Askarbekov Aktan <akn392204@gmail.com>
2024-05-02 18:27:46 +0600

    Add changes in Program and appsettings, also add some changes in layout

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Hw57.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContext<MyTaskContext>(options => options.UseNpgsql(connection));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,11 +22,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=MyTask}/{action=Index}/{id?}");
 
 app.Run();
