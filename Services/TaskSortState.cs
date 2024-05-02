d11597aa529bfd3efe63eba3993eb36d28ba2adf
Askarbekov Aktan <akn392204@gmail.com>
2024-05-02 18:19:32 +0600

    Add TaskSortState enum

---
+++ Services/TaskSortState.cs
@@ -0,0 +1,13 @@
+namespace Hw57.Services;
+
+public enum TaskSortState
+{
+    NameAsc,
+    NameDesc,
+    PriorityAsc,
+    PriorityDesc,
+    StatusAsc,
+    StatusDesc,
+    DateOfCreationAsc,
+    DateOfCreationDesc
+}
